5857f0175d8abf1c241814331c1fcb58dc06b9c8
Ostap Onyshkiv <prostoostap58@gmail.com>
2022-12-15 22:05:02 +0200

    Add project files.

---
+++ lab14/lab14.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream> // std::cout
+#include <algorithm> // std::generate
+#include <vector> // std::vector
+#include <ctime> // std::time
+#include <numeric> // std::accumulate
+using namespace std;
+int RandomNumber() { return (rand() % 100); }
+void print(vector<int> a)
+{
+	for (int i : a)
+		cout << i << " ";
+	cout << endl;
+}
+int main()
+{
+	srand(unsigned(time(0)));
+	int n;
+	cout << "input n : "; cin >> n;
+	vector<int> v(n);
+	generate(v.begin(), v.end(), RandomNumber);
+	print(v);
+	vector<int> u(v.size());
+	auto p = copy_if(v.begin(), v.end(), u.begin(), [](int i) {return i % 2 != 0; });
+	u.resize(p - u.begin());
+	//print(u);
+	auto k = u.size();
+	auto s = accumulate(u.begin(), u.end(), 0);
+	cout << "AVG = " << s * 1. / k << endl;
+	return 0;
+}
